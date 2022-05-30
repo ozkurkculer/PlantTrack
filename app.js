270428ee11df5203b3752d03571e808f9292a7f8
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2022-05-30 22:03:25 +0300

    Add website view contents

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ const bodyParser = require('body-parser');
 const dht11Route = require('./routes/dht11Route');
 const soilRoute = require('./routes/soilRoute');
 
+//Model import
+const dht11 = require('./models/dht11');
+const soil = require('./models/soil');
+
 const app = express();
 
 const port = 3000;
 
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+
 //Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
     console.log(
@@ -20,6 +30,15 @@ app.listen(port, () => {
     );
 });
 
+
+app.get('/',async (req,res) =>{
+    const lastDht = await dht11.findOne().sort({ field: 'asc', _id: -1 }).limit(1);
+    const lastSoil = await soil.findOne().sort({ field: 'asc', _id: -1 }).limit(1);
+    res.render('index', {
+        lastDht,
+        lastSoil
+    });
+});
 app.use('/dht11', dht11Route);
 app.use('/soil', soilRoute);
 
